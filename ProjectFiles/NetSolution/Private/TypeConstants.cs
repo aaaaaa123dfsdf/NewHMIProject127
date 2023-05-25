1286520ff6a8472854c5b0daed33f44ec31071b8
aaaaaa123dfsdf <130623606+aaaaaa123dfsdf@users.noreply.github.com>
2023-05-25 10:09:34 +0800

    Add files via upload

---
+++ ProjectFiles/NetSolution/Private/TypeConstants.cs
@@ -0,0 +1,20 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+namespace NewHMIProject1
+{
+    public static class ObjectTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("NewHMIProject1");
+        public static readonly NodeId MainWindow = new NodeId(namespaceIndex, new Guid("1d3d03d88457d15cf614dd1f9b79b347"));
+    }
+
+    public static class VariableTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("NewHMIProject1");
+    }
+}
---
+++ ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -0,0 +1,11 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "NewHMIProject1", Guid = "1d3d03d88457d15cf614dd1f9b79b347")]
+public class MainWindow : FTOptix.UI.Window
+{
+}
